82a2860be969f66246bc592ad4704643d382ffb9
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-01-12 21:47:19 +0200

    Added forgotten mongoose requirement.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { mongoose } = require('./db/mongoose');
 
 // Load in the mongoose models
 const { List, Task, User } = require('./db/models');
